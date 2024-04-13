4e4f625f23745fcf9e579a4755ab5cc3bc4d4a13
Oleksandr Havrylko <sanya.gavrilko60@gmail.com>
2024-04-13 22:08:18 +0300

    Add repository

---
+++ src/main/java/com/spa/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.spa.repository;
+
+import com.spa.entity.Post;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+    
+}
